f8abfc28930ae75cb6e185bb99dafcc2028d4b7c
SurinJeon <surin0901@naver.com>
2021-01-19 23:41:07 +0900

    sub1

---
+++ sub1/Var02.java
@@ -0,0 +1,18 @@
+package java_study.sub1;
+
+		
+
+public class Var02 {
+
+	
+	public static void main(String[] args) {
+		int yujinbirth = 1995;
+		yujinage(yujinbirth);
+	} // end of main method
+
+	private static void yujinage(int i) {
+		int a = 2021 - i + 1;
+		System.out.println("당신은 " + a + "세 입니다.");
+	} // end of yujinage method
+
+} // end of Var02 Class
