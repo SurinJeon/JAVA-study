07a58ef77529c3d7c1e0450df015f66a42cf66f8
SurinJeon <surin0901@naver.com>
2021-03-08 10:04:31 +0900

    update comment

--- src/java_chobo/ch14/stream/StreamConditionEx.java
+++ src/java_chobo/ch14/stream/StreamConditionEx.java
@@ -12,7 +12,7 @@ public class StreamConditionEx {
 
 		// totalScore가 100이하인 사람은 낙제자 >> 걸러내보기
 		// Match와 Find
-		
+
 		List<Student> list = new ArrayList<Student>();
 		list.add(new Student("이자바", 3, 300));
 		list.add(new Student("김자바", 1, 200));
@@ -21,28 +21,29 @@ public class StreamConditionEx {
 		list.add(new Student("소자바", 1, 95));
 		list.add(new Student("나자바", 3, 290));
 		list.add(new Student("감자바", 3, 80));
-		
+
 		// method로 빼버리기
-		// match
+
+		// 1. match
 		matchEx(list);
-		
-		// find
+
+		// 2. find
 		findEx(list);
-		
+
 	}
 
 	private static void findEx(List<Student> list) {
 		Stream<Student> stdStream = list.stream();
-		
-//		stdStream.filter(s -> s.getTotalScore() <= 100).findFirst(); << 반환타입이 Optional이라서 그거롤 받아야됨
+
+//		stdStream.filter(s -> s.getTotalScore() <= 100).findFirst(); << 반환타입이 Optional이라서 그걸로 받아야됨
 		Optional<Student> result = stdStream.filter(s -> s.getTotalScore() <= 100).findFirst();
 		System.out.println(result); // 100보다 작은 사람 중 첫번째 사람 찾음 (100보다 작은 사람이 없으면 null값을 return)
-		
+
 		// parallel
-		result = list.parallelStream().filter(s -> s.getTotalScore() <= 100).findAny(); // 병렬이기 때문에 순차적으로 실행 X <<findAny! 
+		result = list.parallelStream().filter(s -> s.getTotalScore() <= 100).findAny(); // 병렬이기 때문에 순차적으로 실행 X
+																						// <<findAny!
 		System.out.println(result);
-		
-		
+
 	}
 
 	private static void matchEx(List<Student> list) {
@@ -54,34 +55,15 @@ public class StreamConditionEx {
 				return t.getTotalScore() <= 100; // 100보다 작으면 true임
 			}
 		});
-		
+
 		System.out.printf("has Failed Student? : %s%n", hasFailedStu);
-		
+
 		// 소비했으므로 다시 생성
 		stdStream = list.stream();
-		
+
 		boolean isAllPassed = stdStream.allMatch(s -> s.getTotalScore() >= 80); // 모두가 80 이상이냐? << true
 		System.out.printf("is All Passed? : %s%n", isAllPassed);
-		
-		
-		
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
